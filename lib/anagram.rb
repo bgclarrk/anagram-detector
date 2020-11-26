57b6c096ee1bf0b3e4ebb398c00a4a1dceeb0c7b
+ <+>
2020-11-25 17:24:19 -0700

    commit tree 3258c0fa44f9ffd3c73d93aa0e14a4ba6f84189c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_list)
+        potential_anagram = []
+        @words_list = words_list
+        @words_list.each do |word|
+            potential_anagram << word if word.split("").sort_by!{ |e| e.downcase } == @word.split("").sort_by!{ |e| e.downcase }
+        end
+        potential_anagram.empty? ? [] : potential_anagram
+    end
+end
